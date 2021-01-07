47d3fba7c87bce8d6f72bcab2459122b36e44e44
AnnaMaria Jansen <a.jansen@burningbalance.com>
2021-01-07 11:01:21 +0100

    routes done, unsure about the usage

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users do 
     resources :connections, only:[:index, :show]
-    resources :documents # is it possible to do everything with the documents?
+    # resources :documents # is it possible to do everything with the documents?
   end
   resources :users do
     resources :documents, only:[:index, :show, :create, :destroy, :edit ]
