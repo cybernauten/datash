f94f7561ab703d5322e4222278db1ba2aaafd2f5
AnnaMaria Jansen <a.jansen@burningbalance.com>
2021-01-07 11:45:57 +0100

    no need

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'projects/index'
-  get 'projects/show'
-  get 'projects/edit'
-  get 'projects/update'
-  get 'projects/new'
-  get 'projects/create'
-  get 'projects/destroy'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
