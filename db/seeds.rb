435df768e7cdec2603c381f9781a08cb9bbcb071
AnnaMaria Jansen <a.jansen@burningbalance.com>
2021-01-11 17:50:30 +0100

    seeds user and team

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user_one = User.create!(email: 'soph@example.com', user_name:'sophia' display_name: 'soph' encrypted_password: '123456', avatar_url: "https://avatars0.githubusercontent.com/u/70213059?v=4.jpg")
+file = URI.open('https://images.unsplash.com/photo-1552674605-db6ffd4facb5?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTR8fHJ1bm5pbmd8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60')
+
+user_two = User.create!(email: 'anna@example.com', user_name:'anna-maria' display_name: 'anna' encrypted_password: '123456', avatar_url: "https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1601933188/aowdwrdop6xuksfrwhus.jpg" )
+file = URI.open('https://images.unsplash.com/photo-1523918228128-875ebd63de5b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OXx8cnVubmluZyUyMHRyYWlsfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60')
+
+user_three = User.create!(email: 'ilies@example.com', user_name:'ilies' display_name: 'ilies' encrypted_password: '123456', avatar_url: "https://avatars0.githubusercontent.com/u/68535384?v=4.jpg" )
+file = URI.open('https://images.unsplash.com/photo-1594882645126-14020914d58d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTZ8fHJ1bm5pbmclMjB0cmFpbHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60')
+
+team_one = Team.create!(team_name:'team_one', description: 'hjdksabhdfjkbhjkbhdjs', avatar_url: "https://avatars0.githubusercontent.com/u/68535384?v=4.jpg" )
+file = URI.open('https://images.unsplash.com/photo-1594882645126-14020914d58d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTZ8fHJ1bm5pbmclMjB0cmFpbHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60')
+
+team_two = Team.create!(team_name:'team_two', description: 'DSBAJBDFJSLBDSHJA', avatar_url: "https://avatars0.githubusercontent.com/u/68535384?v=4.jpg" )
+file = URI.open('https://images.unsplash.com/photo-1594882645126-14020914d58d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTZ8fHJ1bm5pbmclMjB0cmFpbHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60')
+
+team_three = Team.create!(team_name:'team_three', description: 'il213543265436T54ies', avatar_url: "https://avatars0.githubusercontent.com/u/68535384?v=4.jpg" )
+file = URI.open('https://images.unsplash.com/photo-1594882645126-14020914d58d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTZ8fHJ1bm5pbmclMjB0cmFpbHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60')
