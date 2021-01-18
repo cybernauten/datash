b8f776d3ca3f1c40f8c68b0660ce328a3e7116ae
Sophia Denney <sophiad14@kabelmail.de>
2021-01-18 09:48:37 +0100

    controller edit method fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,26 @@
 class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
   def index
-    @users = policy_scope(User)
+    #@users = policy_scope(user)
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
     @user = current_user
-    authoriez @user
+    authorize @user
   end
 
   def update
-    #@user = User.find
+    @user = User.find(params[:id])
+    authorize @user
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:display_name, :avatar_url)
   end
 end
