4dbea8ca74a2cf3d12fd0ae51ac187ec23af8178
Sophia Denney <sophiad14@kabelmail.de>
2021-01-19 12:22:40 +0100

    seeds rearranged and teams index

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,8 +1,13 @@
 class TeamsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+  #before_action
+
   def index
+    @teams = policy_scope(Team)
   end
 
   def show
+    @team = Team.find(@team.user_id)
   end
 
   def new
@@ -16,4 +21,9 @@ class TeamsController < ApplicationController
 
   def update
   end
+
+  private
+
+  def team_params
+    params.require(:team).permit(:team_name, :description, :avatar_url)
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,25 +4,25 @@ Team.delete_all
 User.delete_all
 
 user_one = User.create!(email: 'soph2@example.com', user_name:'sophia', display_name: 'soph', password: '123456', avatar_url: "https://avatars0.githubusercontent.com/u/70213059?v=4.jpg")
-
-user_two = User.create!(email: 'anna2@example.com', user_name:'anna-maria', display_name: 'anna', password: '123456', avatar_url: "https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1601933188/aowdwrdop6xuksfrwhus.jpg" )
-
-user_three = User.create!(email: 'ilies2@example.com', user_name:'ilies', display_name: 'ilies', password: '123456', avatar_url: "https://avatars0.githubusercontent.com/u/68535384?v=4.jpg")
-
 team_one = Team.create!(team_name:'team_one', description: 'hjdksabhdfjkbhjkbhdjs', avatar_url: "https://avatars0.githubusercontent.com/u/68535384?v=4.jpg")
-
-team_two = Team.create!(team_name:'team_two', description: 'DSBAJBDFJSLBDSHJA', avatar_url: "https://avatars0.githubusercontent.com/u/68535384?v=4.jpg")
-
-team_three = Team.create!(team_name:'team_three', description: 'il213543265436T54ies', avatar_url: "https://avatars0.githubusercontent.com/u/68535384?v=4.jpg" )
-
 project_one = Project.create!(project_name:'project_one', description: 'hjdksabhdfjkbhjkbhdjs', creation_date: 'date')
-
-project_two = Project.create!(project_name:'project_two', description: 'DSBAJBDFJSLBDSHJA', creation_date: 'date')
-
-project_three = Project.create!(project_name:'project_three', description: 'il213543265436T54ies', creation_date: 'date')
-
 document_one = Document.create!(file_name:'document_one', file_type: '.pdf', creation_date: "date", file_size: '30', project_id: 2, user_id: 1)
 
+user_two = User.create!(email: 'anna2@example.com', user_name:'anna-maria', display_name: 'anna', password: '123456', avatar_url: "https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1601933188/aowdwrdop6xuksfrwhus.jpg" )
+team_two = Team.create!(team_name:'team_two', description: 'DSBAJBDFJSLBDSHJA', avatar_url: "https://avatars0.githubusercontent.com/u/68535384?v=4.jpg")
+project_two = Project.create!(project_name:'project_two', description: 'DSBAJBDFJSLBDSHJA', creation_date: 'date')
 document_two = Document.create!(file_name:'document_two', file_type: '.pdf', creation_date: "date", file_size: '30', project_id: 2, user_id: 2)
 
+user_three = User.create!(email: 'ilies2@example.com', user_name:'ilies', display_name: 'ilies', password: '123456', avatar_url: "https://avatars0.githubusercontent.com/u/68535384?v=4.jpg")
+team_three = Team.create!(team_name:'team_three', description: 'il213543265436T54ies', avatar_url: "https://avatars0.githubusercontent.com/u/68535384?v=4.jpg" )
+project_three = Project.create!(project_name:'project_three', description: 'il213543265436T54ies', creation_date: 'date')
 document_three = Document.create!(file_name:'document_three', file_type: '.pdf', creation_date: "date", file_size: '30', project_id: 3, user_id: 3)
+
+
+
+
+
+
+
+
+
