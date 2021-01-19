b32a7ecbed4cccd49d76217f1b21cf24608de2da
Nikos Agathos <nicolas.agathos@gmail.com>
2021-01-19 16:48:34 +0100

    mini-review seed fix

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,6 @@
 class Team < ApplicationRecord
-  has_many :linkedconnections
-  has_many :users, through: :linkedconnections
+  has_many :linked_connections
+  has_many :users, through: :linked_connections
   has_many :cooperations
   has_many :projects, through: :cooperations
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :teams, through: :linkedconnections
+  has_many :teams, through: :linked_connections
   has_many :documents
 
-  belongs_to :linkedconnection
-  has_many :linkedconnections
+  has_many :linked_connections
 end
