cf440137e1184f52745665479b25ac3f283f7932
AnnaMaria Jansen <a.jansen@burningbalance.com>
2021-01-21 09:43:54 +0100

    user controller done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,29 @@
 class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
+
   def index
+    @users = policy_scope(User)
   end
 
   def show
+    @creator = User.find(params[id])
+    @linked_connection = LinkedConnection.where(team_id: params[:id])
+  end
+
+  def update
+    authorize @user
+  end
+
+  def edit
+    authorize @user
   end
 
   def update
   end
 
-  def edit
+private
+
+  def user_params
+    params.require(:user).permit(:display_name, :user_name, :email, :password, :avatar_url)
   end
 end
