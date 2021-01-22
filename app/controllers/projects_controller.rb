8bd000ffa0b8f39c324bc5c3efaa632f9438430b
Ilies Mebarki <Ilies.mebarki@googlemail.com>
2021-01-22 10:07:37 +0100

    first try of making the project controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,22 +1,31 @@
 class ProjectsController < ApplicationController
   def index
+    #show all projects from current_user he has been assigned to
+    @projects = Project.all.where(Project.user_id == current_user.id)
   end
 
   def show
+    #show one project from current_user he has been assigned to
   end
 
   def edit
+    # change something (most likely a file) in this project where I am assigned to
   end
 
   def update
+    # change something (most likely a file) in this project where I am assigned to
   end
 
   def new
+    #create a new project for the team 
   end
 
   def create
+    #create a new project for the team 
   end
 
   def destroy
+    #destroy a project from team (and all its files???)
+    #question: who should be able to delete a whole project
   end
 end
