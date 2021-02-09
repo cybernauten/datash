3472a54664637e6cd46cb33612f9d9d64ea45441
Ilies Mebarki <Ilies.mebarki@googlemail.com>
2021-02-09 18:13:58 +0100

    Users are shown in a project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
   has_many :cooperations
   has_many :documents
-  has_many :users, through: :assignments
   has_many :assignments
+  has_many :users, through: :assignments
 end
