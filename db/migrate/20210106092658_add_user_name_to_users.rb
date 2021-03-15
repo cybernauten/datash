2551100c33052b94f57daff138a4f149c935348d
Ilies Mebarki <Ilies.mebarki@googlemail.com>
2021-03-15 14:15:49 +0100

    sorry to push in the trunk

---
+++ db/migrate/20210106092658_add_user_name_to_users.rb
@@ -0,0 +1,6 @@
+class AddUserNameToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :user_name, :string, default: email
+    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
+  end
+end
