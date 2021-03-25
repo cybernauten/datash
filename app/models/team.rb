aec1fa9394d8dd6c034202e2a7d43595fb3a5ecb
Ilies Mebarki <Ilies.mebarki@googlemail.com>
2021-03-25 16:55:02 +0100

    Bug fixed

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,6 +2,4 @@ class Team < ApplicationRecord
   has_many :linked_connections, dependent: :destroy
   has_many :users, through: :linked_connections
   has_many :projects, dependent: :destroy
-
-  validates :linked_connection, uniqueness: true
 end
